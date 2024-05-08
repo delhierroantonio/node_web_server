e25aa896cc86e0156937d7f5c67a18c07f000862
antoniodelhierro <delhierroantonio@icloud.com>
2024-05-08 14:51:18 -0600

    modify app.js, server,js and env.js to import and export using ES6 syntax

--- config/env.js
+++ config/env.js
@@ -1,12 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
+import env from 'dotenv'
+env.config()
+import envvar from 'env-var'
+// const { get } = require('env-var')
+// import { get } from 'env-var'
 
-const { get } = require('env-var')
-
-const envs = {
-  PORT: get('PORT').required().asPortNumber(),
-  PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
+export const envs = {
+  PORT: envvar.get('PORT').required().asPortNumber(),
+  PUBLIC_PATH: envvar.get('PUBLIC_PATH').default('public').asString()
 }
 
-module.exports = {
-  envs
-}
+// module.exports = {
+//   envs
+// }
